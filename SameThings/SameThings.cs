6a60c5036e617aa4c1807c03dabb25ed143afc48
Build <jwbradley2003@gmail.com>
2021-02-27 14:27:46 -0500

    Consistency, Version bump
    Removed unused EventHandlers var, bumped version

--- SameThings/SameThings.cs
+++ SameThings/SameThings.cs
@@ -8,13 +8,12 @@ namespace SameThings
     {
         internal static SameThings Instance;
 
-        public override string Name => "SameThings";
-        public override string Author => "Build";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override string Name { get; } = "SameThings";
+        public override string Author { get; } = "Build";
+        public override Version Version { get; } = new Version(1, 0, 1);
         public override Version RequiredExiledVersion { get; } = new Version(2, 3, 4);
 
         private readonly Harmony _harmony = new Harmony(nameof(SameThings).ToLowerInvariant());
-        private readonly EventHandlers _eventHandlers = new EventHandlers();
 
         public override void OnEnabled()
         {
